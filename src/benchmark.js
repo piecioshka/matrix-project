5fa8a76510aa76d067a9034a6fe7accfc2bad3cd
piecioshka <piecioshka@gmail.com>
2012-09-22 22:38:02 +0200

    add benchamrk, first animation

---
+++ src/benchmark.js
@@ -0,0 +1,21 @@
+var Benchmark;
+Benchmark = (function () {
+    "use strict"
+
+    var data = {};
+
+    function print_test_result(name_of_test) {
+        console.log(data[name_of_test] + "ms - " + name_of_test);
+    }
+
+    return {
+        start: function (name_of_test) {
+            data[name_of_test] = (new Date()).getTime();
+        },
+
+        stop: function (name_of_test) {
+            data[name_of_test] = (new Date()).getTime() - data[name_of_test];
+            print_test_result(name_of_test);
+        }
+    };
+}());
--- src/matrix.js
+++ src/matrix.js
@@ -9,10 +9,13 @@ Matrix = (function () {
         CHAR_SIZE_HEIGHT = 10,
 
         settings = {
-            place: null
+            place_id: null
         },
 
-        chars = ["a", "b", 4];
+        area_instance = null,
+
+        chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+            "o", "q", "p", "r", "s", "t", "u", "w", "x", "y", "z"];
 
     // initialize pattern
     (function () {
@@ -21,7 +24,7 @@ Matrix = (function () {
 
     function init_matrix() {
         Matrix.init({
-            place: "#matrix"
+            place_id: "#matrix"
         });
     }
 
@@ -45,31 +48,69 @@ Matrix = (function () {
         return top;
     }
 
-    function create_single_char_for_view() {
+    function get_random_time() {
+        return (Math.random() * 200 + 50).toFixed(0);
+    }
+
+    function create_single_char_for_view(number) {
         var character = document.createElement("span");
         character.innerText = chars[get_random_char()];
         character.className = "character";
-        character.style.left = get_random_left();
-        character.style.top = get_random_top();
+
+        var style = character.style;
+        style.left = number * CHAR_SIZE_WIDTH;
+        style.top = 0;
+        if (number % 2) {
+            style.backgroundColor = "red";
+        }
+
         return character;
     }
 
-    function put_char_on_view(char) {
-        document.querySelector(settings.place).appendChild(char);
+    function put_char_on_view(character, callback) {
+        // optymalization
+        if (area_instance === null) {
+            area_instance = document.querySelector(settings.place_id);
+        }
+        area_instance.appendChild(character);
+        callback(character);
+    }
+
+    function animate_character(character) {
+        var top = 0;
+        var interval = setInterval(function () {
+            if (top >= AREA_SIZE_HEIGHT) {
+                character = null;
+                clearInterval(interval);
+                return false;
+            }
+
+            character.style.top = top + "px";
+            top += 10;
+        }, get_random_time());
     }
 
     function run_matrix() {
         var i;
 
-        for (i = 0; i < 30; i++) {
-            put_char_on_view(create_single_char_for_view());
+        Benchmark.start("create characters");
+
+        for (i = 0; i < AREA_SIZE_WIDTH / CHAR_SIZE_WIDTH; i++) {
+            put_char_on_view(create_single_char_for_view(i), animate_character);
         }
+
+        Benchmark.stop("create characters");
     }
 
     return {
         init: function (config) {
             settings = config;
+
+            Benchmark.start("start of application");
+
             run_matrix();
+
+            Benchmark.stop("start of application");
         }
-    }
+    };
 }());
